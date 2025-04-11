3470009f17839db85ced9540fde6467249d89bd8
sashahelpme <sashaffg@gmail.com>
2025-04-11 18:38:14 +0300

    Update projecttestFile.js

--- projecttestFile.js
+++ projecttestFile.js
@@ -1,19 +1,6 @@
-function test(input){
-const fetch = require('node-fetch');
-  fetch('https://about.pumb.ua/en/info/currency_converter')
-  .then(res => res.text())
-  .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const tableRow = doc.querySelector('#single-page-rate-2 tbody tr:nth-child(2)');
-    
-    const currency = tableRow.children[0].textContent.trim();
-    const purchase = tableRow.children[1].textContent.trim();
-    const sale = tableRow.children[2].textContent.trim();
+function test(){
 
-    const res = ({ currency, purchase, sale });
-  });
-return res;
+return "123";
 }
 
 module.exports = { test };
